c9d99986c48d3995cb2942de10f5019f43d8df1a
rkswetha-xworkz <rkswetha.xworkz@gmail.com>
2024-06-24 10:43:19 +0530

    Commit 1, 24/06/2024

---
+++ Samsung.java
@@ -0,0 +1,20 @@
+class Samsung{
+	public static void main(String[] samsung){
+		System.out.print("Cell Phone is used for Communication. \n");
+		System.out.print("Cell Phone is essential thing in today's life!!");
+		
+		String brand = "Samsung";
+		String operatingSystem = "Android 14";
+		String RAMMemoryInstalledSize = "12 GB";
+		String CPUModel = "Snapdragon";
+		String memoryStorageCapacity = "256 GB";
+		
+		System.out.println("Brand "+brand);
+		System.out.println("Operating System "+operatingSystem);
+		System.out.println("RAM Memory Installed Size "+RAMMemoryInstalledSize);
+		System.out.println("CPU Model "+CPUModel);
+		System.out.println("Memory Storage Capacity "+memoryStorageCapacity);
+	}
+}
+		
+
